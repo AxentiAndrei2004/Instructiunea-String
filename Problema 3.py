54871a79f7654df45b0b7abd34013b5634c4cd22
AxentiAndrei2004 <71274413+AxentiAndrei2004@users.noreply.github.com>
2020-11-25 17:57:35 +0200

    Add files via upload

---
+++ Problema 3.py
@@ -0,0 +1,6 @@
+s1=str(input('Introduceti primul cuvint:'))
+s2=str(input('Introduceti al doilea cuvint:'))
+s3=str(input('Introduceti al treilea cuvint:'))
+s4=str(input('Introduceti al patrulea cuvint:'))
+Fraza=s1 + ' ' + s2 + ' ' + s3 + ' ' + s4 + '.'
+print('Fraza este:', Fraza)
